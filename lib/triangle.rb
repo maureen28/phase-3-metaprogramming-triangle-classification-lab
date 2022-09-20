2a90644a30704ab431350bd80529b64504191baa
maureen <maureen.murimi@moringaschool.com>
2022-09-20 10:01:07 +0300

    commit tree 0d6e09e60a601ab965d33616bf45c8634d92d62c

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,41 @@
 class Triangle
   # write code here
+
+  attr_reader :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    check_validate_triangle
+    if side1 == side2 && side2 == side3
+      :equilateral
+    elsif side1 == side2 || side2 == side3 || side1 == side3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def sides_greater_than_zero?
+    [side1, side2, side3].all?(&:positive?)
+  end
+
+  def valid_triangle_inequality?
+    side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1
+  end
+
+  def check_validate_triangle
+    raise TriangleError unless sides_greater_than_zero? && valid_triangle_inequality?
+  end
+
+  class TriangleError < StandardError
+    def message
+      "This is not a triangle"
+    end
+  end
+
 end
